d7c2780c557909c2cf13e0716b7f5c10943765a5
DeepikaAiluri <deepika4sri@gmail.com>
2019-05-08 13:06:57 -0500

    Initial commit

---
+++ Amsd.java
@@ -0,0 +1,69 @@
+package mathcaluculation;
+
+public class Amsd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Its an assignment program");
+		
+		
+   	 
+   	 int a=5,b=5;
+   	 System.out.println("For multiplication");
+   	 System.out.println("the  result is " + a*b);
+   	 System.out.println("Multiplication end");
+   	 
+ 
+
+	getsum();
+  
+	getdifference();
+	
+	division();
+	
+	System.out.println("End of the main method");
+	
+	
+		
+	}
+
+
+             public  static void getsum(){
+	
+	System.out.println("This is the addition method");
+	int  a=100,b=20;
+	
+	int c=a+b;
+	
+	System.out.println("The total is"+ c);
+	System.out.println("End of the addition");
+}
+
+
+
+public static void getdifference(){
+	 
+System.out.println("Its a subtraction");
+int a=100,b=20;
+int c=a-b;
+System.out.println("the difference is "+ c);
+System.out.println("End of the Subtraction");
+
+
+}
+
+
+
+public static void division(){
+	 
+	 System.out.println("This is a Division program");
+	 int a=10,b=2;
+	 System.out.println("the result is"+ a/b);
+	 
+	 System.out.println("End of the division program ");
+	 
+	 
+}
+
+}
