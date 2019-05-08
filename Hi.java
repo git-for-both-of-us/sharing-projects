7c8b1bf3afa3dacdde1aa2b8d60f96870f9cb8b2
DeepikaAiluri <deepika4sri@gmail.com>
2019-05-08 13:35:25 -0500

    add Hi.java class

---
+++ Hi.java
@@ -0,0 +1,44 @@
+package mathcaluculation;
+
+public   class Hi {
+	
+	 
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Its an assignment program");
+		
+		
+   	 
+		int a=5,  b=5;
+   	 System.out.println("For multiplication");
+   	 System.out.println("the  result is " + (a*b));
+   	 System.out.println("Multiplication end");
+   	 
+ 
+
+	getsum();
+  
+
+	System.out.println("End of the main method");
+	
+	
+		
+	}
+
+
+             public  static void getsum(){
+	
+	System.out.println("This is the addition method");
+	
+	
+	int a=10;int b=15;
+	
+	System.out.println("The total is"+ (a+b));
+	System.out.println("End of the addition");
+}
+
+	
+	
+}
